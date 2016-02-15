4b73f5fcdd2d6204543762112921168cdfa746b6
Mark Vayngrib <mark@tradle.io>
2016-02-15 15:54:34 -0500

    --rm docker container after exit

--- rundocker.sh
+++ rundocker.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker run -it --name tradle-cli -e "HOME=/home" -v $(pwd):/home tradle/cli:1.0.0
+docker run -it --rm --name tradle-cli -e "HOME=/home" -v $(pwd):/home tradle/cli:1.0.0
