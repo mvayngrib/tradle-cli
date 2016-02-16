f6513a6c3682210b53deee74e3ca6a8f5e4dffa9
Mark Vayngrib <mark@tradle.io>
2016-02-15 21:13:21 -0500

    use data volume container

--- rundocker.sh
+++ rundocker.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
-docker run -it --rm --name tradle-cli -e "HOME=/data" -v /data tradle/cli:1.0.0
+# creates a container the first time around
+# ignores error subsequently
+docker create -v /tdata --name tradle-cli-data cogniteev/echo &>/dev/null
+docker run -it --rm --name tradle-cli -e "HOME=/tdata" --volumes-from tradle-cli-data tradle/cli:1.0.0
